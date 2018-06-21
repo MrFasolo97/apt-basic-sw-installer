1f61bde50aa6303735fdf0463e1b8fc3c09e1b6c
MrFasolo97 <79fasolo97@gmail.com>
2018-06-21 20:27:20 +0200

    added fail2ban

--- apt-sw-inst.sh
+++ apt-sw-inst.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y screen zip unzip htop git iotop ufw
+sudo apt-get install -y screen zip unzip htop git iotop ufw fail2ban
